cc676660d26dcc3c0cb001f46db9f237a8553dfa
Devin <devin.johnson@noaa.gov>
2018-01-23 09:52:20 -0800

    mod attendance umbrella code for better behavior opening files

--- HierarchicalGOF/R/get_attendance_code.R
+++ HierarchicalGOF/R/get_attendance_code.R
@@ -6,13 +6,13 @@ run_attendance_analysis = function(show=TRUE, run=FALSE){
   
   dir = system.file("CSL_attendance", package="HierarchicalGOF")
   
-  if(show) file.edit(file.path(dir, "find_post_mode_hsmm.R"))
+  if(show) system(paste0('open "', file.path(dir, "find_post_mode_hsmm.R"), '"'))
   if(run) source(file.path(dir, "find_post_mode_hsmm.R"), local=TRUE)
   
-  if(show) file.edit(file.path(dir, "sample_post_hsmm.R"))
+  if(show) system(paste0('open "', file.path(dir, "sample_post_hsmm.R"), '"'))
   if(run) source(file.path(dir, "sample_post_hsmm.R"), local=TRUE)
   
-  if(show) file.edit(file.path(dir, "attendance_plots.R"), local=TRUE)
+  if(show) system(paste0('open "', file.path(dir, "attendance_plots.R"), '"'))
   if(run) source(file.path(dir, "attendance_plots.R"))
   if(run) system(paste0('open "', file.path(dir, "attendance_fit.pdf"), '"'))
   
